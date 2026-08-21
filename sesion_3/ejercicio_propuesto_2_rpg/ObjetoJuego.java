b38c8379522fb660caf23374ab0bae6498490e92
Jhoaoo Sebastian Llerena Quispe <164796968+jhoaoo@users.noreply.github.com>
2026-08-21 09:43:40 -0500

    Sesion 3 - RPG objeto de juego

---
+++ sesion_3/ejercicio_propuesto_2_rpg/ObjetoJuego.java
@@ -0,0 +1,11 @@
+public class ObjetoJuego {
+    private String nombre;
+
+    public ObjetoJuego(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
