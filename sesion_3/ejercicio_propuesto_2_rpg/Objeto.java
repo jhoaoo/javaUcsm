e38a961697a5cef3610cd42c0b806e135fadde34
Jhoaoo Sebastian Llerena Quispe <164796968+jhoaoo@users.noreply.github.com>
2026-08-21 09:58:14 -0500

    Sesion 3 - RPG agregar clase Objeto

---
+++ sesion_3/ejercicio_propuesto_2_rpg/Objeto.java
@@ -0,0 +1,11 @@
+public class Objeto {
+    private String nombre;
+
+    public Objeto(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
